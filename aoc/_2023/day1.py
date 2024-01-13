d12eef0498c1a379fd4d5e53c9627624ce934779
Alberto Murillo <albertomurillosilva@gmail.com>
2024-01-12 19:07:16 -0800

    2023: day1: refactor

--- aoc/_2023/day1.py
+++ aoc/_2023/day1.py
@@ -1,68 +1,37 @@
 import re
+import sys
+
+
+def solution(data: str, digits: str) -> int:
+    values = {k: i % 9 + 1 for i, k in enumerate(digits.split("|"))}
+    values |= {k: 9 - i % 9 for i, k in enumerate(digits[::-1].split("|"))}
+
+    p1 = re.compile(digits)
+    p2 = re.compile(digits[::-1])
+
+    result = 0
+    for line in data.splitlines():
+        match = p1.search(line)
+        result += values[match[0]] * 10
+
+        match = p2.search(line[::-1])
+        result += values[match[0]]
+
+    return result
 
 
 def part1(data: str) -> int:
-    result = 0
-    for line in data.splitlines():
-        c = next((x for x in line if x.isnumeric()))
-        result += int(c) * 10
-
-        c = next((x for x in reversed(line) if x.isnumeric()))
-        result += int(c)
-
-    return result
+    pattern = "1|2|3|4|5|6|7|8|9"
+    return solution(data, pattern)
 
 
 def part2(data: str) -> int:
-    values = {
-        # Forward values
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-        # Backward values
-        "eno": 1,
-        "owt": 2,
-        "eerht": 3,
-        "ruof": 4,
-        "evif": 5,
-        "xis": 6,
-        "neves": 7,
-        "thgie": 8,
-        "enin": 9,
-        # Numeric values
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
-    }
-    forward_pattern = "one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9"
-    backward_pattern = "9|8|7|6|5|4|3|2|1|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno"
-    result = 0
-    for line in data.splitlines():
-        m = re.search(forward_pattern, line)
-        result += values[m[0]] * 10
-
-        m = re.search(backward_pattern, line[::-1])
-        result += values[m[0]]
-
-    return result
+    pattern = "1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine"
+    return solution(data, pattern)
 
 
 def main():
-    with open("day1.txt", encoding="utf-8") as f:
-        data = f.read()
-
+    data = sys.stdin.read()
     print(f"part 1: {part1(data)}")
     print(f"part 2: {part2(data)}")
 
