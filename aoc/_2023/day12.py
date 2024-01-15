a5a24da07b28360f00a4cf0fb8504c5aea1ddba3
Alberto Murillo <albertomurillosilva@gmail.com>
2024-01-15 00:48:36 -0800

    2023: day 12: rewrite matches

--- aoc/_2023/day12.py
+++ aoc/_2023/day12.py
@@ -6,60 +6,38 @@ from typing import Dict, List, Tuple
 
 class SpringConditions:
     def __init__(self, data: List[str], folded_in_parts: int = 1):
-        self.records: List[Tuple[List[str], List[int]]] = []
+        self.records: List[Tuple[str, List[int]]] = []
         for line in data:
             s, g = line.split()
-            springs = list("?".join(repeat(s, folded_in_parts)))
+            springs = "?".join(repeat(s, folded_in_parts))
             groups = [int(x) for x in ",".join(repeat(g, folded_in_parts)).split(",")]
             self.records.append((springs, groups))
 
-    def matches(self, springs: List[str], groups: List[int]) -> int:
-        # First I solved it with backtracking and failed to solve p2
-        # with memoization.
-        #
-        # Decided to study the fast regular expression approach from this post
-        # https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kd3rclt
-        # https://swtch.com/~rsc/regexp/regexp1.html
-        # https://research.swtch.com/glob
-        matches = 0
+    def matches(self, springs: str, groups: List[int]) -> int:
+        # Based on https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kd3rclt
 
-        cstates: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
-        nstates: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
-        cstates[(0, 0, 0, 0)] = 1
+        nstates: Dict[Tuple[int, int, bool], int] = defaultdict(int)
+        cstates: Dict[Tuple[int, int, bool], int] = defaultdict(int)
+        cstates[(0, 0, False)] = 1
 
-        while len(cstates) > 0:
-            for state, num in cstates.items():
-                # si = springs index
-                # gi = groups index
-                # cc = current count
-                # expdot = expecting a dot
-                si, gi, cc, expdot = state
+        len_groups = len(groups)
 
-                if si == len(springs) and gi == len(groups):
-                    matches += num
+        for spring in springs:
+            for (gi, cc, want_dot), seen in cstates.items():
+                if spring in ".?" and cc == 0:
+                    nstates[(gi, 0, False)] += seen
 
-                if si == len(springs):
-                    continue
-
-                if (springs[si] in "#?") and gi < len(groups) and expdot == 0:
-                    if springs[si] == "?" and cc == 0:
-                        nstates[(si + 1, gi, cc, expdot)] += num
+                if spring in "#?" and gi < len_groups and not want_dot:
                     cc += 1
                     if cc == groups[gi]:
-                        gi += 1
-                        cc = 0
-                        expdot = 1
-                    nstates[(si + 1, gi, cc, expdot)] += num
-                    continue
-
-                if (springs[si] in ".?") and cc == 0:
-                    expdot = 0
-                    nstates[(si + 1, gi, cc, expdot)] += num
+                        nstates[(gi + 1, 0, True)] += seen
+                    else:
+                        nstates[(gi, cc, False)] += seen
 
             cstates, nstates = nstates, cstates
             nstates.clear()
 
-        return matches
+        return sum(v for (gi, _, _), v in cstates.items() if gi == len_groups)
 
 
 def part1(data: str):
