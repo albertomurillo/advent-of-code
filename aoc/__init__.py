48f093fe74ee61b129f8f41fb245ad744fa40b6a
Alberto Murillo <albertomurillosilva@gmail.com>
2024-01-16 15:32:19 -0800

    aoc: Add graphs module

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -1,5 +1,16 @@
 from typing import List
 
+from aoc.graphs import Graph
+from aoc.grids import Grid
+
 
 def as_matrix(data: str) -> List[List[str]]:
     return [list(x) for x in data.splitlines()]
+
+
+def as_graph(grid: Grid) -> Graph:
+    graph = Graph()
+    for e1, w in grid.items():
+        for e2 in [n for n in e1.neighbors if n in grid]:
+            graph.add_vertex(e1, e2, int(grid[e2]))
+    return graph
---
+++ aoc/graphs.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from typing import Hashable
+
+from aoc.heaps import BucketQueue
+
+Edge = Hashable
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = defaultdict(dict)
+
+    def add_vertex(self, e1: Edge, e2: Edge, w=0):
+        self.vertices[e1][e2] = w
+
+    def dijkstra(self, start: Edge, target: Edge) -> int:
+        q = BucketQueue()
+        q.push(0, start)
+        visited = set()
+
+        while q:
+            cost, e1 = q.pop()
+
+            if e1 in visited:
+                continue
+            visited.add(e1)
+
+            if e1 == target:
+                return cost
+
+            for e2, w in self.vertices[e1].items():
+                q.push(cost + w, e2)
+
+        return -1
