10d2535fe46d5b7c94fa5fe4a9bead32f1043d6f
Alberto Murillo <albertomurillosilva@gmail.com>
2024-12-08 13:14:03 -0800

    2024: day 8

---
+++ src/aoc/_2024/day8.py
@@ -0,0 +1,80 @@
+import sys
+from collections import defaultdict
+from itertools import combinations
+
+Point = tuple[int, int]
+Vector = tuple[int, int]
+Frequency = str
+FrequencyMap = dict[Frequency, list[Point]]
+
+
+def next_point(point: Point, vector: Vector) -> Point:
+    return point[0] + vector[0], point[1] + vector[1]
+
+
+def vector(p1: Point, p2: Point) -> Vector:
+    return p2[0] - p1[0], p2[1] - p1[1]
+
+
+def is_inrange(p: Point, rows: int, cols: int) -> bool:
+    return 0 <= p[0] <= rows and 0 <= p[1] <= cols
+
+
+def parse_input(data: str) -> tuple[int, int, FrequencyMap]:
+    frequency_map = defaultdict(list)
+    row = col = 0
+    for row, line in enumerate(data.splitlines()):
+        for col, c in enumerate(line):
+            if c.isalnum():
+                frequency_map[c].append((row, col))
+    return row, col, frequency_map
+
+
+def part1(data: str) -> int:
+    rows, cols, frequency_map = parse_input(data)
+
+    antinodes = set()
+    for antennas in frequency_map.values():
+        for a, b in combinations(antennas, 2):
+            v = vector(a, b)
+            if is_inrange((p := next_point(b, v)), rows, cols):
+                antinodes.add(p)
+
+            v = vector(b, a)
+            if is_inrange((p := next_point(a, v)), rows, cols):
+                antinodes.add(p)
+
+    return len(antinodes)
+
+
+def part2(data: str) -> int:
+    rows, cols, frequency_map = parse_input(data)
+
+    antinodes = set()
+    for antennas in frequency_map.values():
+        for a, b in combinations(antennas, 2):
+            p, v = a, vector(a, b)
+            while True:
+                p = next_point(p, v)
+                if not is_inrange(p, rows, cols):
+                    break
+                antinodes.add(p)
+
+            p, v = b, vector(b, a)
+            while True:
+                p = next_point(p, v)
+                if not is_inrange(p, rows, cols):
+                    break
+                antinodes.add(p)
+
+    return len(antinodes)
+
+
+def main() -> None:
+    data = sys.stdin.read()
+    print(f"part 1: {part1(data)}")
+    print(f"part 2: {part2(data)}")
+
+
+if __name__ == "__main__":
+    main()
